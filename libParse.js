db6f0b481ab07c896e6f640213b229f0ddb27027
Vanillyn <skyykiwi@gmail.com>
2023-01-26 20:31:52 -0500

    what

--- libParse.js
+++ libParse.js
@@ -3,5 +3,10 @@ const csv=require('csvtojson')
 csv()
 .fromFile(csvFilePath)
 .then((jsonObj)=>{
-    console.log(jsonObj);
-})
+    for (const [key, value] of Object.entries(jsonObj)) {
+        // console.log(value)
+        const json = value
+        console.log(key, json.Name)
+
+      }
+});
