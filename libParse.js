b593e34304cb6f1a3d9b4ba860e76f1373d5275c
Vanillyn <skyykiwi@gmail.com>
2023-01-26 21:20:07 -0500

    update

--- libParse.js
+++ libParse.js
@@ -1,12 +1,8 @@
-const csvFilePath='library.csv'
-const csv=require('csvtojson')
+const lib='library.csv'
+const csv=require('csvtojson');
 csv()
-.fromFile(csvFilePath)
+.fromFile(lib)
 .then((jsonObj)=>{
-    for (const [key, value] of Object.entries(jsonObj)) {
-        // console.log(value)
-        const json = value
-        console.log(key, json.Name)
-
-      }
+        const json = Object.entries(jsonObj)
+        console.dir(json, {'maxArrayLength': null});
 });
